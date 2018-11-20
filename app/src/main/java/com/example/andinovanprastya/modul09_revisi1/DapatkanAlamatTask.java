753f4f78411a5634800ca9a67cd076b1fc52794f
andinovanprastya <andinovan@gmail.com>
2018-11-20 22:33:28 +0700

    Praktikum 10.2

--- app/src/main/java/com/example/andinovanprastya/modul09_revisi1/DapatkanAlamatTask.java
+++ app/src/main/java/com/example/andinovanprastya/modul09_revisi1/DapatkanAlamatTask.java
@@ -1,53 +1,34 @@
 package com.example.andinovanprastya.modul09_revisi1;
-
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
-import android.os.Message;
 import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
-
+import com.google.android.gms.location.places.Place;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-public class DapatkanAlamatTask extends AsyncTask<Location, Void, String> {
-
+public class DapatkanAlamatTask  extends AsyncTask<Location, Void, String> {
     private Context mContext;
     private onTaskSelesai mListener;
-
     DapatkanAlamatTask(Context applicationContext, onTaskSelesai listener){
         mContext = applicationContext;
         mListener = listener;
     }
     @Override
-    protected void onPostExecute(String alamat) {
-        mListener.onTaskCompleted(alamat);
-        super.onPostExecute(alamat);
-
-    }
-
-    @Override
     protected String doInBackground(Location... locations) {
 //        return null;
-
         // object geocorder
         Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
-
         // object location mengambil object pertama saja dari location karena kita hanya ingin mengambil 1 alamat saja
         Location location = locations[0];
-
         // list object address
         List<Address> alamat = null ;
-
         // untuk menyimpan hasil alamat resultMessage
         String resultMessage = "";
-
         // try catch digunakan untuk mendapat data list alamat dari obj Location yg menangkap
         // error jika tdk ada jaringan
         try{
@@ -56,10 +37,8 @@ public class DapatkanAlamatTask extends AsyncTask<Location, Void, String> {
                     location.getLongitude(),1 );
         } catch (IOException ioException){
             resultMessage = "service tidak tersedia";
-
             // parameter ke 3 adalah jml maks alamat
             Log.e("lokasiError", resultMessage, ioException);
-
             // catch ini untuk menangkap error ketika long dan lat invalid
         } catch (IllegalArgumentException illegalArgumentException){
             resultMessage = "koordinat invalid";
@@ -77,23 +56,21 @@ public class DapatkanAlamatTask extends AsyncTask<Location, Void, String> {
         }else {
             Address address = alamat.get(0);
             ArrayList<String> barisAlamat = new ArrayList<>();
-
             //dapatkan baris alamat menggunakan fungsi getAddressLine dan gabungkan
             for(int i = 0; i<= address.getMaxAddressLineIndex();i++){
                 barisAlamat.add(address.getAddressLine(i));
             }
-
             //gabungkan line alamat dipisah baris baru
             resultMessage = TextUtils.join("\n",barisAlamat );
         }
-
-
         return resultMessage;
     }
-
-
+    @Override
+    protected void onPostExecute(String alamat) {
+        mListener.onTaskCompleted(alamat);
+        super.onPostExecute(alamat);
+    }
     interface onTaskSelesai{
         void onTaskCompleted(String result);
     }
-
 }
--- app/src/main/java/com/example/andinovanprastya/modul09_revisi1/MainActivity.java
+++ app/src/main/java/com/example/andinovanprastya/modul09_revisi1/MainActivity.java
@@ -15,172 +15,231 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.andinovanprastya.modul09_revisi1.DapatkanAlamatTask;
+import com.example.andinovanprastya.modul09_revisi1.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements DapatkanAlamatTask.onTaskSelesai{
-
-    private static final int REQUEST_LOCATION_PERMISSION = 1;
-    private Button mLocationButton;
-    private TextView mLocationTextView;
+    private Button btn;
     private Location mLastLocation;
-    private FusedLocationProviderClient mFusedLocationClient;
-    private ImageView mAndroidImageView;
-    private AnimatorSet mRotateAnim;
-    private boolean mTrackingLocation;
-    private FusedLocationProviderClient getmFusedLocationClient;
     private LocationCallback mLocationCallback;
-
-
+    private TextView mLocationTextView;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private AnimatorSet mRotateAnimator;
+    private ImageView mAndroidImageView;
+    private boolean mTrackingLocation;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    private  String mLastPlaceName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLocationButton = (Button) findViewById(R.id.button_location);
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mLocationTextView = (TextView) findViewById(R.id.textview_location);
+        btn = (Button) findViewById(R.id.button_location);
         mAndroidImageView = (ImageView) findViewById(R.id.imageview_android);
 
-        //Animasi
-        mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator
-                (this, R.animator.rotate);
-        mRotateAnim.setTarget(mAndroidImageView);
-
-
-        mLocationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View v) {
-                if (!mTrackingLocation) {
-                    mulaiTrackingLokasi();
-                }else{
-                    stopTrackingLokasi();
-                }
-//                mulaiTrackingLokasi();
-            }
-        });
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-
-        mLocationCallback = new LocationCallback() {
+        mLocationCallback = new LocationCallback(){
             @Override
             public void onLocationResult(LocationResult locationResult) {
-                // jika tracking aktif, proses reverse menjadi data alamat
-                if (mTrackingLocation){
-                    new DapatkanAlamatTask(MainActivity.this,
-                            MainActivity.this)
+                //                super.onLocationResult(locationResult);
+                // apabila tracking aktif, proses geocode jadi data alamat.
+                if(mTrackingLocation){
+                    new DapatkanAlamatTask(MainActivity.this, MainActivity.this)
                             .execute(locationResult.getLastLocation());
                 }
             }
         };
-    }
 
+        // buat animasi
+        mRotateAnimator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate );
+        mRotateAnimator.setTarget(mAndroidImageView);
 
-    private void mulaiTrackingLokasi() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]
-                            {Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
-        } else {
-//            Log.d("GETPERMISI", "mulaiTrackingLokasi: permissions granted");
-//
-            mFusedLocationClient.requestLocationUpdates
-                    (getLocationRequest(),mLocationCallback,null);
-//            mFusedLocationClient.getLastLocation().addOnSuccessListener(
-//                    new OnSuccessListener<Location>() {
-//                        @Override
-//                        public void onSuccess(Location location) {
-//                            if (location != null) {
-//                                // lakukan reverse geocode AsyncTask
-//                                new DapatkanAlamatTask(MainActivity.this,
-//                                        MainActivity.this).execute(location);
-//                            } else {
-//                                mLocationTextView.setText("Lokasi tidak tersedia");
-//                            }
-////                            if (location != null){
-////                                mLastLocation = location;
-////                                mLocationTextView.setText(
-////                                        getString(R.string.location_text,
-////                                                mLastLocation.getLatitude(),
-////                                                mLastLocation.getLongitude(),
-////                                                mLastLocation.getTime()));
-////                            } else {
-////                                mLocationTextView.setText("Lokasi tidak tersedia");
-////                            }
-//                        }
-//                    }
-//            );
-        }
-
-        mLocationTextView.setText(getString(R.string.alamat_text,
-                "Sedang mencari alamat doi",
-                System.currentTimeMillis()));
-
-        mTrackingLocation = true;
-        mLocationButton.setText("Stop Tracking Lokasi");
-        mRotateAnim.start();
-
-    }
-
-
-    @Override
-
-    public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult) {
-
-        switch (requestCode) {
-            case REQUEST_LOCATION_PERMISSION:
-                // jika permission diijinkan, mulaiTrackingLokasi()
-                // jika tidak, tampilkan Toast
-
-                if (grantResult.length > 0
-                        && grantResult[0] == PackageManager.PERMISSION_GRANTED){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //                getLocation();
+                //                mulaiTrackingLokasi();
+                if (!mTrackingLocation){
                     mulaiTrackingLokasi();
                 } else {
-                    Toast.makeText(this,
-                            "Permission bapakknya gak dapet masbro, mesakke", Toast.LENGTH_SHORT).show();
+                    stopTrackingLokasi();
                 }
+                mLocationTextView.setText(getString(R.string.alamat_text,
+                        "sedang mencari nama tempat",
+                        "sedang mencari alamat",
+                        System.currentTimeMillis()));
+                mTrackingLocation = true;
+                btn.setText("Stop tracking lokasi");
+            }
+        });
+    }
+
+    //    private void getLocation(){
+    //        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+    //                != PackageManager.PERMISSION_GRANTED){
+    //            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION} ,
+    //                    REQUEST_LOCATION_PERMISSION );
+    //        } else {
+    ////            Log.d("GETPERMISSION", "getLocation : permission granted");
+    //            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+    //                    new OnSuccessListener<Location>() {
+    //                        @Override
+    //                        public void onSuccess(Location location) {
+    //                            if(location != null){
+    ////                                get lang long
+    //
+    ////                                mLastLocation = location;
+    ////                                mLocationTextView. setText(
+    ////                                        getString(R.string.location_text,
+    ////                                                mLastLocation.getLatitude(),
+    ////                                                mLastLocation.getLongitude(),
+    ////                                                mLastLocation.getTime())
+    ////                                );
+    //                                new DapatkanAlamatTask(MainActivity.this, MainActivity.this).execute(location);
+    //                            } else {
+    //                                mLocationTextView.setText("Lokasi tidak tersedia");
+    //                            }
+    //                        }
+    //                    }
+    //            );
+    //        }
+    //        mLocationTextView.setText(getString(R.string.alamat_text, "sedang mencari alamat",
+    //                System.currentTimeMillis()));
+    //
+    //    }
+
+    //end
+    //
+
+    private void mulaiTrackingLokasi(){
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION} ,
+                    REQUEST_LOCATION_PERMISSION );
+        } else {
+            //            Log.d("GETPERMISSION", "getLocation : permission granted");
+
+            mFusedLocationClient.requestLocationUpdates
+                    (getLocationRequest(), mLocationCallback,null );
+            mLocationTextView.setText(getString(R.string.alamat_text, "sedang mencari alamat",
+                    System.currentTimeMillis()));
+            // mLocationTextView.setText(getString(R.string.alamat_text, "sedang mencari alamat",
+            //   System.currentTimeMillis()));
+            mTrackingLocation = true;
+            btn.setText("Stop Tracking Lokasi");
+            mRotateAnimator.start();
         }
     }
 
+
+
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_LOCATION_PERMISSION:
+
+                // jika permission dibolehkan maka getlocation,jika tidak dibolehkan, tampilkan peringatan
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mulaiTrackingLokasi();
+                } else {
+                    Toast.makeText(this, "tidak dapat permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
     @Override
     public void onTaskCompleted(String result) {
-        // update ui dengan tampilan hasil alamat
-//        mLocationTextView.setText(getString(R.string.alamat_text,result,System.currentTimeMillis()));
-        if (mTrackingLocation) {
+        public void onTaskCompleted(final String result) throws SecurityException {
+
+            if (mTrackingLocation) {
+//                Task<PlaceLikelihoodBufferResponse> placeResult = mPlaceDetectionClient.getCurrentPlace(null);
+                Task<PlaceLikelihoodBufferResponse> placeResult = mPlaceDetectionClient.getCurrentPlace(null);
+                placeResult.addOnCompleteListener(
+                        new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                            @Override
+                            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                                if(task.isSuccessful()){
+                                    PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                                    float maxLikelihood = 0;
+                                    Place currentPlace = null;
+                                    for (PlaceLikelihood placeLikelihood : likelyPlaces){
+                                        if(maxLikelihood < placeLikelihood.getLikelihood()){
+                                            maxLikelihood = placeLikelihood.getLikelihood();
+                                            currentPlace = placeLikelihood.getPlace();
+                                        }
+                                    }
+                                    if (currentPlace !=null){
+                                        mLocationTextView.setText(
+                                                getString(R.string.alamat_text,
+                                                        currentPlace.getName(),
+                                                        result, System.currentTimeMillis())
+                                        );
+                                        likelyPlaces.release();
+                                    } else {
+                                        mLocationTextView.setText(
+                                                getString(R.string.alamat_text,
+                                                        "nama lokasi tidak ditemukan!",
+                                                        result,
+                                                        System.currentTimeMillis())
+                                        );
+                                    }
+                                }
+                            }
+                        });
+                mPlaceDetectionClient.getCurrentPlace(null);
+                // menampilkan alamat
+                // mLocationTextView.setText(getString(R.string.alamat_text, result, System.currentTimeMillis()));
+            }
+        }
+        //        mLocationTextView.setText(getString(R.string.alamat_text, result, System.currentTimeMillis()));
+        //
+        if(mTrackingLocation){ // untuk cek mTrackingLocatin aktif / tdk
             mLocationTextView.setText(getString(R.string.alamat_text,
                     result, System.currentTimeMillis()));
         }
     }
-
     private void stopTrackingLokasi(){
-        if (mTrackingLocation) {
+        if(mTrackingLocation){
             mTrackingLocation = false;
-            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-            mLocationButton.setText("Mulai Tracking Lokasi");
+
+
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback); // menghapus req update location
+
+            btn.setText("Mulai Tracking Lokasi");
             mLocationTextView.setText("Tracking sedang dihentikan");
-            mRotateAnim.end();
+            mRotateAnimator.end();
         }
+
     }
 
+    // digunakan untuk menentukan frekuensi req dan tingkat akurasi dari update lokasi
     private LocationRequest getLocationRequest(){
         LocationRequest locationRequest = new LocationRequest();
-
-        // digunakan untuk seberapa sering update lokasi yg diinginkan
-        locationRequest.setInterval(10000);
-
-        // adalah seberapa sering update lokasi dari app lain yg meminta req lokasi
-        locationRequest.setFastestInterval(5000);
-
-        // parameter untuk memilih akurasi dan akurasi tinggi menggunakan GPS
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);  //  untuk seberapa sering update lokasi yg diinginkan
+        locationRequest.setFastestInterval(5000); // seberapa sering update lokasi dari app lain yang minta req location
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // untuk memilih akurasi tinggi menggunakan GPS
         return locationRequest;
     }
+
 }
